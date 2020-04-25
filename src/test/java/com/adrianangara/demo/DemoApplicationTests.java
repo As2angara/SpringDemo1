5e6f2f2b8c86af2808529ad855057221d06abf67
AdrianSA <Adrian.angara@gmail.com>
2020-04-24 21:47:36 -0400

    First commit

---
+++ src/test/java/com/adrianangara/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianangara.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
